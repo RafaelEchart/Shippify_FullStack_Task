913bd10d488fb3ac1182f64a196bae577e85ff06
Rafael Echart <rafaelechartsaltos@gmail.com>
2022-04-26 16:06:24 -0500

    Backend: CORS settings and Not found route messagg

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,26 @@ const app = express();
 
 app.use(bodyParser.json());
 
+//CORS option for the backend
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+  
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  
+    next();
+  });
+
+
+  //Routes not found
+app.use((req, res, next) => {
+    const error = new HttpError("Could not find this route", 404);
+    throw error;
+  });
+  
 
 app.listen(3001, () => {
     console.log(`App running on port 3001.`)

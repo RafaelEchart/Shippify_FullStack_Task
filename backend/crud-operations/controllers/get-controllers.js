5762973257c28e81557e1026426b664c0eb78811
Rafael Echart <rafaelechartsaltos@gmail.com>
2022-04-26 18:11:06 -0500

    backend: first connection attempt

--- backend/crud-operations/controllers/get-controllers.js
+++ backend/crud-operations/controllers/get-controllers.js
@@ -1,9 +1,27 @@
+const mysql = require('mysql');
+
+//Database Connection
+const conn = mysql.createConnection({
+  host: `${process.env.DATABASE_HOST}`,
+  user: `${process.env.DATABASE_USERNAME}`,
+  password: `${process.env.DATABASE_PASSWORD}`,
+  database: `${process.env.DATABASE_SCHEMA}`
+});
+
 
 const getDriversController = async (req, res, next) => {
+
+  let sqlQuery = "SELECT * FROM driver JOIN vehicle ON vehicle.driver_id = driver.id";
   
-  res.status(200).json({
-    message: "This is working!"
+  
+  let query = conn.query(sqlQuery, (err, results) => {
+    if(err) throw err;
+    res.status(200).json({
+      results
+    });
   });
+  
+ 
 };
 
 
